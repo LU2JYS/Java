74ec7652bb650789907bae52fdc3299523dfb5a0
전용성 <162073406+LU2JYS@users.noreply.github.com>
2024-06-17 17:38:51 +0900

    20240617 Java

--- 0617/src/p0617/p0617_16.java
+++ 0617/src/p0617/p0617_16.java
@@ -15,7 +15,7 @@ public class p0617_16 {
             System.out.println(dan + "단:");
             for (int i = 1; i <= 9; i++) {
                 System.out.println(dan + " x " + i + " = " + (dan * i));
-            }
+            } 
 		
 		
 		
