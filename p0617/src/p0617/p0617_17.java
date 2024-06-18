18d1b0f09a278fa6d0d9caae8f181e3d02e11d2a
전용성 <162073406+LU2JYS@users.noreply.github.com>
2024-06-18 18:13:21 +0900

    20240618 Java

--- p0617/src/p0617/p0617_17.java
+++ p0617/src/p0617/p0617_17.java
@@ -7,7 +7,7 @@ public class p0617_17 {
     public static void main(String[] args) {
     	//1-100까지, 랜덤숫자 10개를 생성해서, 10개의 숫자의 합을 출력하시요
         Random random = new Random();
-        int sum = 0;
+        int sum = 0; //합계
 
         for (int i = 0; i < 10; i++) {
             int randomNumber = 1 + random.nextInt(100); // nextInt(100) gives 0-99, so add 1 to make it 1-100
