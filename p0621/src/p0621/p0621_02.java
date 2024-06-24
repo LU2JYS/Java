ac9755da3c242641658a1c42c94ea025eb97b096
전용성 <162073406+LU2JYS@users.noreply.github.com>
2024-06-24 18:20:43 +0900

    240624 Java

--- p0621/src/p0621/p0621_02.java
+++ p0621/src/p0621/p0621_02.java
@@ -1,20 +1,13 @@
-package p0624;
-
+package p0621;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
-
 public class Stu_process {
 	
+    static Scanner scan = new Scanner(System.in); //클래스변수
 	
-int s_count = 0; //Stu_score count.사용
-	
-	Scanner scan = new Scanner(System.in);
-	String[] title = {"학번","이름","국어","영어","수학","합계","평균","등수"};
-	String stuNo,name; 
-	int kor,eng,math,total,rank;
-	double avg;
-	
-	//화면부분 메소드
-	int screen() {
+	// 1. 화면부분
+    int screen() {
 		//화면부분
 		System.out.println("[ 학생성적 프로그램 ]");
 		System.out.println("---------------------");
@@ -27,22 +20,30 @@ int s_count = 0; //Stu_score count.사용
 		System.out.println("---------------------");
 		System.out.println("원하는 번호를 입력하세요.>> ");
 		int choice = scan.nextInt();
-		scan.nextLine(); //enter키 때문
-		
+		scan.nextLine();
 		return choice;
 	}//screen
 	
-	//학생성적입력부분
-	void stu_input(stu_score[] sc) {
+	// 1. 학생성적입력부분 - Stu_score[] sc,int s_count
+	int stu_input(Stu_score[] sc,int s_count) {
 		while(true) {
-			s_count = stu_score.count; //Stu_score count사용
 			System.out.println("[ 학생 성적입력 ]");
+			
+			//학번자동생성
+			Date date = new Date();
+			SimpleDateFormat sdf = new SimpleDateFormat("yy");
+			String stuNo1 = "S"+ sdf.format(date) + String.format("%03d", s_count+1);
+			// 객체선언
+			sc[s_count] = new Stu_score();
+			// 학번저장
+			sc[s_count].stuNo = stuNo1;
+			System.out.println("학생번호 : "+stuNo1);
 			// 이름저장
 			System.out.println("이름을 입력하세요.>>(0.이전페이지 이동) ");
-			name = scan.nextLine();
+			sc[s_count].name = scan.nextLine();
 			
 			//이전페이지 이동
-			if(name.equals("0")) {
+			if(sc[s_count].name.equals("0")) {
 				System.out.println("이전페이지로 이동합니다.!!");
 				System.out.println();
 				break;
@@ -50,27 +51,29 @@ int s_count = 0; //Stu_score count.사용
 			
 			// 국어,영어,수학점수 저장
 			System.out.println("국어점수를 입력하세요.>> ");
-			kor = scan.nextInt();
+			sc[s_count].kor = scan.nextInt();
 			System.out.println("영어점수를 입력하세요.>> ");
-			eng = scan.nextInt();
+			sc[s_count].eng = scan.nextInt();
 			System.out.println("수학점수를 입력하세요.>> ");
-			math = scan.nextInt();
+			sc[s_count].math = scan.nextInt();
 			scan.nextLine();
-			
-			// 객체선언 - 매개변수가 있는 생성자 사용
-			sc[s_count] = new stu_score(name,kor,eng,math);
+			//합계,평균 저장
+			sc[s_count].total = sc[s_count].kor+sc[s_count].eng+sc[s_count].math;
+			sc[s_count].avg = sc[s_count].total/3.0;
 			
 			//입력완료
 			System.out.println("< 학생성적 입력완료 >");
-			System.out.printf("%s 학생성적이 저장되었습니다.",name);
+			System.out.printf("%s 학생성적이 저장되었습니다.",sc[s_count].name);
 			System.out.println();
-			
-		}
-		System.out.println();
+			s_count++;
+		}//while
+		
+		return s_count;
+		
 	}//stu_input
 	
-	//학생성적출력부분
-	void stu_output(stu_score[] sc) {
+	// 2. 학생성적입력부분 - Stu_score[] sc
+	void stu_output(Stu_score[] sc,int s_count,String[] title) {
 		System.out.println("                    [ 학생 성적출력 ]");
 		for(int i=0;i<title.length;i++) {
 			System.out.printf("%s\t",title[i]);
@@ -91,26 +94,6 @@ int s_count = 0; //Stu_score count.사용
 		System.out.println();
 	}//stu_output
 	
-	//학생성적등수
-	void stu_rank(stu_score[] sc) {
-		System.out.println("[ 등수처리 ]");
-		for(int i=0;i<s_count;i++) {
-			int count = 1; //초기화
-			for(int j=0;j<s_count;j++)
-				if(sc[i].total<sc[j].total) count++;
-			sc[i].rank = count;
-		}
-		System.out.println("등수처리가 완료되었습니다.!!");
-		System.out.println();
-	}//stu_rank
+	
+	
 }//class
-
-
-
-
-
-
-
-
-
-
